1566819ab2345e02d1c31baafa076e795f2d1f6f
Adeel Tariq <adeel.tariq@web.de>
2020-07-16 16:07:34 +0200

    funtion to put deposit in account added

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,19 @@
 package org.xpdojo.bank;
 
 public class Account {
+
+    int amount = 0;
+
+    public Account () {
+
+    }
+
+    public int checkAccount() {
+        return amount;
+    }
+
+    public void deposit(int amount) {
+        amount = amount;
+    }
+
 }
